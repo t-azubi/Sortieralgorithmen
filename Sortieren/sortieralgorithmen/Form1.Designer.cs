4174cbba0bbb17d138348660d116c23008fa6e35
Dominik Mangatter <40673554+Zeroks77@users.noreply.github.com>
2019-08-16 11:11:05 +0200

    add project

---
+++ Sortieren/sortieralgorithmen/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace TestMalen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(679, 452);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw Unsorted";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Draw BubbleSort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(685, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Draw QuickSort";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(685, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(685, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Wie groß soll die \r\nzu sortierende Liste sein?";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(685, 376);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Draw MergeSort";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(688, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(685, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 30);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Zeit die der Algorithmus\r\ngebraucht hat.";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(685, 415);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Draw ShellSort";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Best Case",
+            "Average Case",
+            "Worst Case"});
+            this.listBox1.Location = new System.Drawing.Point(685, 191);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 43);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(682, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Sec: DezSec: MiliSec:MicroSec";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Sortieren/sortieralgorithmen/Form1.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TestMalen
+{
+    public partial class Form1 : Form
+    {
+        Chart chart = new Chart();
+        int[] unsorted;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = " ";
+            int y;
+            int.TryParse(textBox1.Text, out y);
+            chart1.Series.Clear();
+            var listIndex = listBox1.SelectedIndex;
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Series1",
+                Color = System.Drawing.Color.Green,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Stock
+
+            };
+            chart1.ResetAutoValues();
+            this.chart1.Series.Add(series1);
+            int[] blub = new int[y];
+            if (listIndex == 0)
+            {
+                blub = bestCase(y);
+            }
+            else if (listIndex == 1)
+            {
+                blub = averageCase(y);
+            }
+            else if (listIndex ==2)
+            {
+                blub = worstCase(y);
+            }
+            for (int i = 0; i < y; i++)
+            {
+                series1.Points.AddXY(i, blub[i]);
+            }
+            chart1.Invalidate();
+            chart = chart1;
+            unsorted = blub;
+        }
+        private int[] averageCase(int y)
+        {
+            Random ran = new Random();
+            int[] iInput = new int[y];
+            for (int i = 0; i < y; i++)
+            {
+                iInput[i] = i + 1;
+            }
+            for (int i = 0; i < y; i++)
+            {
+                var random = ran.Next(0, y);
+                var temp = iInput[i];
+                iInput[i] = random;
+                iInput[random] = temp;
+            }
+            return iInput;
+        }
+        private int[] worstCase(int y)
+        {
+            List<int> List = new List<int>();
+            for (int i = 0; i < y; i++)
+            {
+                List.Add(i);
+            }
+            List.Reverse();
+            return List.ToArray();
+        }
+        private int[] bestCase(int y)
+        {
+            List<int> List = new List<int>();
+            for (int i = 0; i < y; i++)
+            {
+                List.Add(i);
+            }
+            return List.ToArray();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            int y;
+            int.TryParse(textBox1.Text, out y);
+            Sorting sort = new Sorting();
+            chart1.Series.Clear();
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Series1",
+                Color = System.Drawing.Color.Green,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Stock
+            };
+            this.chart1.Series.Add(series1);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var array = sort.BubbleSortMethod(unsorted);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                ts.Ticks / 10000000,
+                ts.Ticks / 100000, ts.Ticks / 1000, ts.Ticks / 10);
+            textBox2.Text = elapsedTime;
+            for (int i = 0; i < y; i++)
+            {
+                series1.Points.AddXY(i, array[i]);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int y;
+            int.TryParse(textBox1.Text, out y);
+            Sorting sort = new Sorting();
+            chart1.Series.Clear();
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Series1",
+                Color = System.Drawing.Color.Green,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Stock
+            };
+            this.chart1.Series.Add(series1);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var array = sort.QuickSortMethod(unsorted);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                ts.Ticks / 10000000,
+                ts.Ticks / 100000, ts.Ticks / 1000, ts.Ticks / 10);
+            textBox2.Text = elapsedTime;
+            for (int i = 0; i < y; i++)
+            {
+                series1.Points.AddXY(i, array[i]);
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            int y;
+            int.TryParse(textBox1.Text, out y);
+            Sorting sort = new Sorting();
+            chart1.Series.Clear();
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Series1",
+                Color = System.Drawing.Color.Green,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Stock
+            };
+            this.chart1.Series.Add(series1);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var array = sort.MergeSort(unsorted);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                 ts.Ticks / 10000000,
+                 ts.Ticks / 100000, ts.Ticks / 1000, ts.Ticks / 10);
+            textBox2.Text = elapsedTime;
+            for (int i = 0; i < y; i++)
+            {
+                series1.Points.AddXY(i, array[i]);
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            int y;
+            int.TryParse(textBox1.Text, out y);
+            Sorting sort = new Sorting();
+            chart1.Series.Clear();
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Series1",
+                Color = System.Drawing.Color.Green,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Stock
+            };
+            this.chart1.Series.Add(series1);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var array = sort.ShellSort(unsorted);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                ts.Ticks / 10000000,
+                ts.Ticks / 100000, ts.Ticks / 1000, ts.Ticks / 10);
+            textBox2.Text = elapsedTime;
+            for (int i = 0; i < y; i++)
+            {
+                series1.Points.AddXY(i, array[i]);
+            }
+        }
+
+    }
+
+    class Sorting
+    {
+        public int[] BubbleSortMethod(int[] iInput)
+        {
+
+            for (int i = 0; i < iInput.Length; i++)
+            {
+                for (int j = 0; j < iInput.Length - 1; j++)
+                {
+                    if (iInput[j] > iInput[j + 1])
+                    {
+                        int temp = iInput[j];
+                        iInput[j] = iInput[j + 1];
+                        iInput[j + 1] = temp;
+                    }
+                }
+            }
+            return iInput;
+        }
+        public int[] QuickSortMethod(int[] iInput)
+        {
+            iInput = Quicksort(iInput, 0, iInput.Length - 1);
+            return iInput;
+        }
+
+        public static int[] Quicksort(int[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            var pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    var tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(elements, left, j);
+            }
+
+            if (i < right)
+            {
+                Quicksort(elements, i, right);
+            }
+
+            return elements;
+        }
+        public int[] MergeSortStart(int[] input, int low, int high)
+        {
+            if (low < high)
+            {
+                int middle = (low / 2) + (high / 2);
+                MergeSortStart(input, low, middle);
+                MergeSortStart(input, middle + 1, high);
+                Merge(input, low, middle, high);
+            }
+            return input;
+        }
+
+        public int[] MergeSort(int[] input)
+        {
+            input = MergeSortStart(input, 0, input.Length - 1);
+           return input;
+        }
+
+        private int[] Merge(int[] input, int low, int middle, int high)
+        {
+
+            int left = low;
+            int right = middle + 1;
+            int[] tmp = new int[(high - low) + 1];
+            int tmpIndex = 0;
+
+            while ((left <= middle) && (right <= high))
+            {
+                if (input[left] < input[right])
+                {
+                    tmp[tmpIndex] = input[left];
+                    left = left + 1;
+                }
+                else
+                {
+                    tmp[tmpIndex] = input[right];
+                    right = right + 1;
+                }
+                tmpIndex = tmpIndex + 1;
+            }
+
+            if (left <= middle)
+            {
+                while (left <= middle)
+                {
+                    tmp[tmpIndex] = input[left];
+                    left = left + 1;
+                    tmpIndex = tmpIndex + 1;
+                }
+            }
+
+            if (right <= high)
+            {
+                while (right <= high)
+                {
+                    tmp[tmpIndex] = input[right];
+                    right = right + 1;
+                    tmpIndex = tmpIndex + 1;
+                }
+            }
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                input[low + i] = tmp[i];
+            }
+            return input;
+        }
+        public int[] ShellSort(int[] array)
+        {
+            int n = array.Length;
+            int gap = n / 2;
+            int temp;
+
+            while (gap > 0)
+            {
+                for (int i = 0; i + gap < n; i++)
+                {
+                    int j = i + gap;
+                    temp = array[j];
+
+                    while (j - gap >= 0 && temp < array[j - gap])
+                    {
+                        array[j] = array[j - gap];
+                        j = j - gap;
+                    }
+
+                    array[j] = temp;
+                }
+
+                gap = gap / 2;
+            }
+            return array;
+        }
+    }
+}
